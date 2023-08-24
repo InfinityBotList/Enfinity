bc6b0b6ec24b0cd2248df321ab172e18f284623a
Rootspring <cheesycod@outlook.com>
2023-08-24 22:30:57 +0530

    Update devs.js

--- source/database/pool/devs.js
+++ source/database/pool/devs.js
@@ -1,8 +1,12 @@
 const Pool = require('pg').Pool
 
-module.exports = global.pool = new Pool({
+global.pool = new Pool({
     connectionString: process.env.DEVS_PG,
     connectionTimeoutMillis: 5000,
     idleTimeoutMillis: 10000,
     max: 10
 })
+
+global.pool.on('drain', global.pool.end.bind(client));
+
+module.exports = global.pool
