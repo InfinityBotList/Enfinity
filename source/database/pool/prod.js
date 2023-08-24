539590628718dd47d6d0618085e13746931815d1
Rootspring <cheesycod@outlook.com>
2023-08-24 22:30:15 +0530

    maybe?

--- source/database/pool/prod.js
+++ source/database/pool/prod.js
@@ -1,8 +1,13 @@
 const Pool = require('pg').Pool
 
-module.exports = global.pool = new Pool({
+global.pool = new Pool({
     connectionString: process.env.PROD_PG,
     connectionTimeoutMillis: 5000,
     idleTimeoutMillis: 10000,
     max: 10
 })
+
+
+global.pool.on('drain', client.end.bind(client));
+
+module.exports = global.pool
